8ebd2c778cb67ec4e8069502f1efe0f1dce58fac
Anthony <anthony.jose53@gmail.com>
2022-06-02 12:52:02 -0400

    Add basic structure for add, working on edit and new

---
+++ models/mangas.js
@@ -0,0 +1,103 @@
+//Sakamoto Days. Tokyo Ghoul/re, Pluto, One Punch Man, 20th Century Boys, Vagabond, Berserk, Akira, Naruto,
+
+mangas = [{
+        Name: "Hajime No Ippo",
+        Arthur: "Joji Morikawa",
+        Genre:"Boxing",
+        Demographic: "Shonen",
+        Volumes: "134",
+        img: "https://i.imgur.com/ubAEbwT.jpeg",
+        Rating:"9"
+    },
+    {
+        Name:"Sakamoto Days",
+        Arthur:"Yuto Suzuki",
+        Genre:"Action Comedy",
+        Demographic:"Shonen",
+        Volumes:"7",
+        img: "https://i.imgur.com/OcBAvy4.jpeg",
+        Rating:"8.5",
+
+    },
+    {
+        Name:"Tokyo Ghoul",
+        Arthur:"Sui Ishida",
+        Genre:"Supernatural Thriller",
+        Demographic:"Seinen",
+        Volumes:"14",
+        img: "https://i.imgur.com/l81pb3z.jpeg",
+        Rating:"8.7",
+
+    },
+    {
+        Name:"Pluto",
+        Arthur:"Naoki Urasawa",
+        Genre:"Mystery Thriller",
+        Demographic:"Seinen",
+        Volumes:"8",
+        img: "https://i.imgur.com/kFApy6D.jpeg",
+        Rating:"10",
+
+    },
+    {
+        Name:"One Punch Man",
+        Arthur:"ONE",
+        Genre:"Action",
+        Demographic:"Seinen",
+        Volumes:"25",
+        img: "https://i.imgur.com/B4pcLEm.jpeg",
+        Rating:"8.7",
+
+    },
+    {
+        Name:"20th Century Boys",
+        Arthur:"Naoki Urasawa",
+        Genre:"Mystery",
+        Demographic:"Seinen",
+        Volumes:"22",
+        img: "https://i.imgur.com/UuTJmXg.jpeg",
+        Rating:"9.0",
+
+    },
+    {
+        Name:"Berserk",
+        Arthur:"Kentaro Miura",
+        Genre:"Dark Fantasy",
+        Demographic:"Seinen",
+        Volumes:"41",
+        img: "https://i.imgur.com/OMqKuBI.jpeg",
+        Rating:"9.2",
+
+    },
+    {
+        Name:"Vagabond",
+        Arthur:"Takehiko Inoue",
+        Genre:"Historical",
+        Demographic:"Seinen",
+        Volumes:"37",
+        img: "https://i.imgur.com/tQphGa6.jpeg",
+        Rating:"10",
+
+    },
+    {
+        Name:"Akira",
+        Arthur:"Katsuhiro Otomo",
+        Genre: "Cyberpunk",
+        Demographic:"Seinen",
+        Volumes:"6",
+        img: "https://i.imgur.com/E2i70mV.jpeg",
+        Rating:"8.5",
+
+    },
+    {
+        Name:"Naruto",
+        Arthur:"Masahi Kishimoto",
+        Genre:"Adventure",
+        Demographic:"Shonen",
+        Volumes:"72",
+        img: "https://i.imgur.com/DHQdpxB.jpeg",
+        Rating:"9.6",
+
+    },
+]
+module.exports = mangas;
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const Mangas = require("./models/mangas.js");
+const methodOverride = require("method-override");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = express();
+//-------------------------------------------------------------------------
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+//--------------------------------------------------------------------------
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+//----------------------------------------------------------------
+//ROUTES-----------INDEX-----------------------
+app.get('/manga', (req, res) => {
+    res.render("index.ejs", {
+         allManga : Mangas
+    })
+});
+
+//----------------NEW------------------------
+app.get('/manga/new', (req, res) => {
+    res.render("new.ejs")
+});
+
+//--------------DELETE--------------------------
+app.delete("/manga/:index", (req, res) => {
+    Mangas(req.params.index, 1)
+    res.redirect('/manga')
+})
+
+//------------- UPDATE-------------------------
+app.put('/manga/:index', (req, res) =>{
+    Mangas[req.params.index].Rating = req.body.Rating
+    res.redirect('/manga')
+})
+
+//-------------CREATE--------------------------
+app.post("/manga" , (req, res) =>{
+    newEntry = {
+        Name: req.body.Name,
+        Arthur: req.body.Arthur,
+        Genre: req.body.Genre,
+        img: "https://i.imgur.com/ZahwJjN.gif",
+        Demographic: req.body.Demographic,
+        Volumes: req.body.Volumes,
+    }
+    console.log(newEntry);
+    Mangas.push(newEntry)
+    res.redirect("/manga")
+})
+
+//---------------EDIT--------------------------
+app.get('/Manga/:index/edit', (req, res) =>{
+    res.render('edit.ejs',{
+        manga: Mangas[req.params.index],
+        Name: req.params.Name
+    })
+})
+
+//--------------SHOW---------------------------
+app.get('/Manga/:index', (req, res) => {
+    res.render('show.ejs', {
+        theManga: Mangas[req.params.index],
+        Name: req.params.Name
+    });
+})
+
+// Listenter
+app.listen(3000, () => {
+    console.log(`The server is listening on port: ${3000}`)
+})
